c050b403e809f2a6c4a7a40eb7b7ccf216b23ce3
_McClayn <79939728+Mc-Clayn@users.noreply.github.com>
2026-02-17 21:17:52 +0300

    Update _constants.py

--- python/gui/netradio/_constants.py
+++ python/gui/netradio/_constants.py
@@ -15,10 +15,10 @@ DEFAULT_CONFIG = {
 	"version": 1,
 	"channels": [
 		{
-			"displayName": "MTRadio", # netradio.by/wot
+			"displayName": "MTRadio", # netradio.by/wot 
 			"stream_url": "https://listen8.myradio24.com/2187", # http://sv.netradio.by:8061/128
-			"ext_url": "https://listen8.myradio24.com/2187", # https://netradio.by/wot
-			"tags_url": "http://dilyaso1.beget.tech/mtradio/" # http://sv.netradio.by:81/broad.xml
+			"ext_url": "https://mtradio.by/", # https://netradio.by/wot
+			"tags_url": "https://mtradio.by/mtradio/" # http://sv.netradio.by:81/broad.xml
 		}
 	]
 }
